a550195733d55ff655dc870052abdc5e3456777b
Luizsan <luizsanq@gmail.com>
2017-06-17 12:16:47 -0300

    Fixed code inputs for SSMC, they now should work in all gametypes
    Still not sure what was the cause of the bug, but rearranging them in metrics seems to have fixed the issue

--- Scripts/Input.lua
+++ Scripts/Input.lua
@@ -1,5 +1,23 @@
 --//================================================================
 
+local gamecodes = {
+    ["Options"] = {
+        ["dance"]   = "Left,Right,Left,Right,Left,Right",
+        ["pump"]    = "DownLeft,DownRight,DownLeft,DownRight,DownLeft,DownRight",
+        ["default"] = "MenuLeft,MenuRight,MenuLeft,MenuRight,MenuLeft,MenuRight"
+    }
+}
+
+function GameCode(code)
+    if gamecodes and gamecodes[code] and gamecodes[code][string.lower(Game())] then
+        return gamecodes[code][string.lower(Game())];
+    else
+        return gamecodes[code]["default"];
+    end;
+end;
+
+--//================================================================
+
 local function MenuInputGame(button)
     local game = string.lower(Game());
     local inputs = {
@@ -39,7 +57,7 @@ function MenuInputActor()
         MenuLeftP2MessageCommand=function(self,param) MESSAGEMAN:Broadcast("MenuInput", { Input = MenuInputGame("MenuLeft"), Button = "Left", Player = PLAYER_2 }); end; 
         MenuRightP1MessageCommand=function(self,param) MESSAGEMAN:Broadcast("MenuInput", { Input = MenuInputGame("MenuRight"), Button = "Right", Player = PLAYER_1 }); end; 
         MenuRightP2MessageCommand=function(self,param) MESSAGEMAN:Broadcast("MenuInput", { Input = MenuInputGame("MenuRight"), Button = "Right", Player = PLAYER_2 }); end; 
-        CodeMessageCommand=function(self,param) MESSAGEMAN:Broadcast("MenuInput", { Input = string.gsub(param.Name, "_", ""), Button = "", Player = param.PlayerNumber }); end;
+        CodeMessageCommand=function(self,param) MESSAGEMAN:Broadcast("MenuInput", { Input = param.Name, Button = "", Player = param.PlayerNumber }); end;
     }
 end;
 
