de531974114e73b38ef15f8010e5443bd6e67b72
Luizsan <luizsanq@gmail.com>
2017-06-19 23:01:15 -0300

    Gameplay assets!

---
+++ BGAnimations/ScreenGameplay overlay/assets/lifebar.lua
@@ -0,0 +1,270 @@
+local spacing = 16;
+local width = 0;
+local height = 12;
+local pn = nil;
+local tick_width = 2;
+local cornersize = 6;
+local corneradjust = 1;
+
+local t = Def.ActorFrame{
+    BuildMessageCommand=function(self,param)
+        pn = param.Player;
+        local gameplay = SCREENMAN:GetTopScreen();
+        local player = gameplay:GetChild("Player" .. ToEnumShortString(pn))
+        local field = player:GetChild("NoteField");
+        local dist = math.abs(player:GetX() - _screen.cx);
+        local style = string.lower(GAMESTATE:GetCurrentStyle():GetName());
+    
+        if style == "double" or style == "routine" then
+            width = _screen.w - (spacing*4);
+        else
+            width = (dist*2) - (spacing*2);
+        end;
+
+        self:x(player:GetX());
+        self:y(SCREEN_TOP+24);
+    end;
+
+    -- base
+    Def.Quad{
+        InitCommand=cmd(diffuse,0.2,0.2,0.2,1);
+        BuildCommand=function(self,param)
+            self:zoomto(width+2,height+4);
+        end;
+    },
+
+    -- background
+    LoadActor(THEME:GetPathG("","lifebg"))..{
+        BuildCommand=function(self,param)
+            self:zoomto(width,height);
+            self:diffuse(BoostColor(PlayerColor(pn),0.2));
+            self:diffusetopedge(BoostColor(PlayerColor(pn),0.1));
+            self:customtexturerect(0,0,width/16,1); 
+        end;
+
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                if param.LifeMeter:IsInDanger() and not param.LifeMeter:IsFailing() then
+                    self:diffuseshift();
+                    self:effectcolor1(0.25,0,0,1);
+                    self:effectcolor2(0.50,0,0,1);
+                    self:effectperiod(0.2);
+                elseif param.LifeMeter:IsFailing() then
+                    self:diffuseshift();
+                    self:effectcolor1(0.15,0.1,0.1,1);
+                    self:effectcolor2(0.15,0.1,0.1,1);
+                    self:effectperiod(1);
+                else
+                    self:stopeffect();
+                end
+            end;
+        end;
+    },
+
+    -- mask
+    Def.Quad{
+        InitCommand=cmd(MaskSource;diffuse,1,0,0,1);
+        EffectCommand=cmd(bounce;effectmagnitude,-math.min(width,480) * pnSide(pn) * 0.175,0,0;effecttiming,0,0,1,0,0;effectclock,"bgm");
+        BuildCommand=function(self,param)
+            if style == "double" or style == "routine" then
+                self:horizalign(right);
+            else
+                self:horizalign(pnAlign(OtherPlayer[pn]));
+            end;
+            self:zoomto(math.min(width,480) * 0.175,height);
+            self:playcommand("Effect");
+        end;
+
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                local life = param.LifeMeter:GetLife();
+                if life < 1 then
+                    self:playcommand("Effect");
+                    self:visible(true);
+                else
+                    self:stopeffect();
+                    self:visible(false);
+                end;
+                self:x(width * pnSide(pn) * ((1 - (life*2))*0.5));
+            end;
+        end;
+    },
+
+    -- sub meter
+    Def.Quad{
+        InitCommand=cmd(diffuseshift;effecttiming,0,0,1,0,0;effectclock,"bgm";blend,Blend.Add);
+        BuildCommand=function(self,param)
+            if style == "double" or style == "routine" then
+                self:x(-width*0.5);
+                self:horizalign(left);
+            else
+                self:x(width * 0.5 * pnSide(pn));
+                self:horizalign(pnAlign(pn));
+            end;
+            self:zoomto(width * 0.5,height);
+            self:effectcolor2(BoostColor(PlayerColor(pn),1));
+            self:effectcolor1(0,0,0,0);
+        end;
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                local life = param.LifeMeter:GetLife();
+                self:zoomto(width * life, height);
+                if param.LifeMeter:IsInDanger() then
+                    self:effectcolor2(1,0,0,1);
+                    self:effectcolor1(1,0,0,0);
+                else
+                    self:effectcolor2(BoostColor(PlayerColor(pn),1));
+                    self:effectcolor1(0,0,0,0);
+                end;
+            end;
+        end;
+    },
+
+    -- meter
+    Def.Quad{
+        InitCommand=cmd(MaskDest);
+        BuildCommand=function(self,param)
+            if style == "double" or style == "routine" then
+                self:x(-width*0.5);
+                self:horizalign(left);
+            else
+                self:x(width * 0.5 * pnSide(pn));
+                self:horizalign(pnAlign(pn));
+            end;
+            self:zoomto(width * 0.5,height);
+            self:diffuse(BoostColor(PlayerColor(pn),1));
+            self:diffusebottomedge(BoostColor(HighlightColor(),1.25));
+        end;
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                local life = param.LifeMeter:GetLife();
+                self:zoomto(width * life, height);
+            end;
+        end;
+    },
+
+    -- wave
+    LoadActor(THEME:GetPathG("","wave"))..{
+        InitCommand=cmd(blend,Blend.Add);
+        BuildCommand=function(self,param)
+            if style == "double" or style == "routine" then
+                self:x(-width*0.5);
+                self:horizalign(left);
+            else
+                self:x(width * 0.5 * pnSide(pn));
+                self:horizalign(pnAlign(pn));
+            end;
+            self:zoomto(width,height+8);
+            self:customtexturerect(0,0,width/256,1);
+            self:texcoordvelocity(0.3,0);
+            self:diffuse(BoostColor(PlayerColor(pn),1.75));
+            self:diffusealpha(0);
+        end;
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                local life = param.LifeMeter:GetLife();
+                self:stoptweening();
+                if life == 1 then
+                    self:linear(0.5);
+                    self:diffusealpha(1);
+                else
+                    self:diffusealpha(0);
+                end;
+            end;
+        end;
+    },
+
+
+    --bars
+    Def.ActorFrame{
+        BuildCommand=cmd(diffuse,0.6,0.6,0.6,1);
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                if param.LifeMeter:IsInDanger() and not param.LifeMeter:IsFailing() then
+                    self:diffuseshift();
+                    self:effectcolor1(1,0.75,0.75,1);
+                    self:effectcolor2(1,0.5,0.5,1);
+                    self:effectperiod(1);
+                elseif param.LifeMeter:IsFailing() then
+                    self:diffuseshift();
+                    self:effectcolor1(0.75,0.3,0.3,1);
+                    self:effectcolor2(0.75,0.3,0.3,1);
+                    self:effectperiod(1);
+                elseif param.LifeMeter:IsHot() then
+                    self:diffuseshift();
+                    self:effectcolor1(1,1,1,1);
+                    self:effectcolor2(1,1,1,1);
+                    self:effectperiod(0.5);
+                else
+                    self:stopeffect();
+                end
+            end;
+        end;
+
+        -- center
+        LoadActor(THEME:GetPathG("","lifeborder"))..{
+            InitCommand=cmd(animate,false;setstate,1;y,-0.5);
+            BuildCommand=function(self,param)
+                self:zoomto(width - cornersize + (corneradjust*2),height+10);
+            end;
+        },
+
+        --corner left
+        LoadActor(THEME:GetPathG("","lifeborder"))..{
+            InitCommand=cmd(animate,false;setstate,0;halign,0;y,-0.5);
+            BuildCommand=function(self,param)
+                self:zoomto(cornersize,height+10);
+                self:x(-width*0.5 - (cornersize*0.5) - corneradjust);
+            end;
+        },
+
+        --corner right
+        LoadActor(THEME:GetPathG("","lifeborder"))..{
+            InitCommand=cmd(animate,false;setstate,0;halign,0;y,-0.5);
+            BuildCommand=function(self,param)
+                self:zoomto(-cornersize,height+10);
+                self:x(width*0.5 + (cornersize*0.5) + corneradjust);
+            end;
+        },
+
+    },
+
+    -- tick
+    Def.ActorFrame{
+        LifeChangedMessageCommand=function(self,param)
+            if param.Player == pn then
+                local life = param.LifeMeter:GetLife();
+                self:visible(not param.LifeMeter:IsFailing());
+                self:x((width-tick_width) * pnSide(pn) * ((1 - (life*2))*0.5));
+            end;
+        end;
+
+
+        LoadActor(THEME:GetPathG("","tick"))..{
+            InitCommand=cmd(diffuseshift;effecttiming,0,0,1,0,0;effectclock,"bgm";blend,Blend.Add);
+            BuildCommand=cmd(zoomto,12,height+8;effectcolor2,PlayerColor(pn);effectcolor1,1,1,1,0);
+            LifeChangedMessageCommand=function(self,param)
+                if param.Player == pn then
+                    if param.LifeMeter:IsFailing() then
+                        self:diffuse(1,0,0,1);
+                        self:stopeffect();
+                    end;
+                end;
+            end;
+        },
+
+        Def.Quad{
+            BuildCommand=cmd(zoomto,tick_width,height);
+        }, 
+
+
+    },
+
+
+
+
+
+};
+
+
+return t;
--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,292 +1,109 @@
-local style;
-local master;
-local source;
+local paused = false;
 
-if (Var "LoadingScreen") == "ScreenThemeSettings" then
-	source = PLAYER_2;
-	style = 'TwoPlayersTwoSides';
-	master = PLAYER_2;
-else
-	source = GAMESTATE:GetHumanPlayers();
-	style = GAMESTATE:GetCurrentStyle():GetStyleType();
-	master = GAMESTATE:GetMasterPlayerNumber();
+local function Update(self,dt)
+    MESSAGEMAN:Broadcast("Update");
 end;
-
-local posY = SCREEN_CENTER_Y+(THEME:GetMetric("Player","ReceptorArrowsYStandard"))-46;
-
-local lifeP1 = 0.5
-local lifeP2 = 0.5
-local lifeMaster = 0.5
-
 
 local t = Def.ActorFrame{
-	InitCommand=cmd(SetUpdateFunction,UpdateGameplay);
+	InitCommand=cmd(SetUpdateFunction,Update);
 	OnCommand=function() Global.disqualified = false; end;
-	PausedMessageCommand=function(self) Global.disqualified = true; end;
-	LifeChangedMessageCommand=function(self,params)
-	
-		if params.Player == PLAYER_1 then
-			lifeP1=params.LifeMeter:GetLife();
-		end
-		if params.Player == PLAYER_2 then
-			lifeP2=params.LifeMeter:GetLife();
-		end
-		
-		if master == PLAYER_1 then lifeMaster = lifeP1 
-		elseif master == PLAYER_2 then lifeMaster = lifeP2 end;
-
-	end;
-
+	PausedMessageCommand=function() Global.disqualified = true; paused = true; end;
+	UnpausedMessageCommand=function() paused = false; end;
+	LifeChangedMessageCommand=function(self,param) end;
 }
 
---[[
+for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
+	if SideJoined(pn) then
 
-for pn in ivalues(source) do
+		-- detail top
+		t[#t+1] = LoadActor(THEME:GetPathG("","hex decoration"))..{
+			InitCommand=cmd(diffuse,0.8,0.8,0.8,0.2;zoomx,0.8 * -pnSide(pn);zoomy,0.4;x,_screen.cx + (_screen.w/2 * pnSide(pn));horizalign,left;vertalign,top);		
+		};
 
-	if Var "LoadingScreen" == "ScreenGameplay" then
-	t[#t+1] = LoadActor("decoration")..{
-		InitCommand=cmd(horizalign,left;vertalign,top;zoomy,0.475;blend,Blend.Add;diffuse,0.75,0.9,1,0.66;diffusebottomedge,0.66,0.85,1,0.25);
-		OnCommand=function(self)
-			if pn == PLAYER_1 then self:zoomx(0.725); self:x(SCREEN_LEFT); elseif pn == PLAYER_2 then self:x(SCREEN_RIGHT); self:zoomx(-0.725) end;
-		end;
-	};
-	
-	end;
-end
+		-- detail bottom
+		t[#t+1] = LoadActor(THEME:GetPathG("","hex decoration"))..{
+			InitCommand=cmd(diffuse,0.8,0.8,0.8,0.15;zoomx,0.5 * -pnSide(pn);zoomy,-0.3;x,_screen.cx + (_screen.w/2 * pnSide(pn));y,_screen.h;horizalign,left;vertalign,top);
+		};
 
+		-- glow
+		t[#t+1] = LoadActor(THEME:GetPathG("","glow"))..{
+			InitCommand=cmd(diffuse,PlayerColor(pn);diffusealpha,0.2;zoomto,512,160;x,_screen.cx + (_screen.w/2 * pnSide(pn));;skewx,0.75*pnSide(pn));
+		};
 
-if style == 'StyleType_OnePlayerTwoSides' or 
-	style == 'StyleType_TwoPlayersSharedSides' then
-	
-			t[#t+1] = LoadActor("basemeter")..{
-				InitCommand=cmd(y,posY-8;zoomx,0.45;zoomy,-0.45;CenterX;zoomx,(SCREEN_WIDTH-90)/400;diffuse,0.4,0.4,0.4,0.7;animate,false);
-				LifeChangedMessageCommand=function(self) if lifeMaster <= 0.2 then self:setstate(1); else self:setstate(0); end; end;
-			};
-			
-			t[#t+1] = Def.Quad{
-				InitCommand=cmd(y,posY;x,SCREEN_CENTER_X;zoomto,100,20;horizalign,left;bounce;effectmagnitude,(SCREEN_WIDTH/15)*-1,0,0;effectclock,"bgm";effecttiming,1,0,0,0;MaskSource;);
-				LifeChangedMessageCommand=function(self)
-					self:x((lifeMaster-0.5)*(SCREEN_WIDTH-90) + (SCREEN_WIDTH/2));
-							if lifeMaster==1 then
-								self:visible(false);
-							else
-								self:visible(true);
-							end
-				end;
-			};
-
-			t[#t+1] = LoadActor("basemeter")..{
-				InitCommand=cmd(y,posY+5;zoom,0.475;x,50;horizalign,left;zoomx,(SCREEN_WIDTH-90)/400;cropright,0.5;MaskDest;blend,Blend.Add;animate,false);
-				LifeChangedMessageCommand=function(self)
-				self:cropright(1-lifeMaster);
-				end;
-			};
-
-			t[#t+1] = LoadActor("basemeter")..{
-				InitCommand=cmd(x,50;y,posY+5;horizalign,right;zoom,0.475;;zoomx,10/400;blend,Blend.Add;animate,false);
-			};
-			
-			
-			
-				t[#t+1] = LoadActor("hot_lores")..{
-					InitCommand=cmd(y,math.ceil(posY);CenterX;diffuse,0.5,0.75,0.95,0);
-					OnCommand=cmd(zoomto,SCREEN_WIDTH-90,39.1;customtexturerect,0,0,1,1;blend,Blend.Add;texcoordvelocity,0.33,0);
-					LifeChangedMessageCommand=function(self) if lifeMaster == 1 then self:stoptweening(); self:decelerate(1); self:diffusealpha(1);
-					else self:stoptweening(); self:decelerate(0.1); self:diffusealpha(0); end;
+		--player
+		t[#t+1] = Def.ActorFrame{
+			InitCommand=cmd(y,SCREEN_BOTTOM-22;x,_screen.cx + (_screen.w/2 * pnSide(pn)) + (12 * -pnSide(pn)));
+			-- player name
+			Def.BitmapText{
+				Font = Fonts.gameplay["Main"];
+				InitCommand=cmd(zoom,0.5;strokecolor,0.15,0.15,0.15,0.4;horizalign,pnAlign(pn);vertalign,bottom);
+				OnCommand=function(self)
+					local name = PROFILEMAN:GetProfile(pn):GetDisplayName();
+					if name == "" then
+						self:diffusealpha(0.33);
+						self:settext("No Profile");
+					else
+						self:diffusealpha(1);
+						self:settext(name);
 					end;
-				};
-				
-				
-				
-			for pn=1,2 do
+				end;
+			},
 
-								t[#t+1] = LoadActor("mask")..{
-								InitCommand=cmd(y,posY;zoomx,0.475;zoomy,0.42;horizalign,left);
-								OnCommand=function(self) 
-										if pn == 1 then self:x(SCREEN_LEFT-10);
-										elseif pn == 2 then self:x(SCREEN_RIGHT+10); end;
-										self:zoomx(0.475*pnSide(pn)*-1);
-									end;
-								};
-								
-								t[#t+1] = LoadActor("corner")..{
-									InitCommand=cmd(y,posY-0.5;zoom,0.375;horizalign,left);
-									OnCommand=function(self) 
-										if pn == 1 then self:x(SCREEN_LEFT+40);
-										elseif pn == 2 then self:x(SCREEN_RIGHT-40); end;
-										self:zoomx(0.375*pnSide(pn)*-1);
-									end;
-									LifeChangedMessageCommand=function(self) if lifeMaster <= 0.2 then self:diffuseshift(); self:effectcolor1(1,1,1,1); self:effectcolor2(1,0.5,0.33,1); self:effectperiod(0.05); else self:stopeffect(); end; end;
-								};
-			end
-		
-			
-								t[#t+1] = LoadActor("frame")..{
-									InitCommand=cmd(y,posY-0.5;CenterX);
-									OnCommand=cmd(zoomto,SCREEN_WIDTH-155,100*0.375;customtexturerect,0,0,1,1);
-									LifeChangedMessageCommand=function(self) if lifeMaster <= 0.2 then self:diffuseshift(); self:effectcolor1(1,1,1,1); self:effectcolor2(1,0.5,0.33,1); self:effectperiod(0.05); else self:stopeffect(); end; end;
-								};
-								
-							t[#t+1] = LoadActor("tip")..{
-									InitCommand=cmd(y,math.ceil(posY)+4;animate,false;;blend,Blend.Add);
-									OnCommand=cmd(zoomy,0.475;zoomx,0.666);
-									LifeChangedMessageCommand=function(self) 
-									if lifeMaster <= 0.2 then self:setstate(1) self:visible(true) else self:setstate(0) end;
-									self:x((lifeMaster-0.5)*(SCREEN_WIDTH-102) + (SCREEN_WIDTH/2));
-									end;
-							};
-							
-							t[#t+1] = LoadActor("tip")..{
-									InitCommand=cmd(y,math.ceil(posY)+4;animate,false;diffusealpha,0.25);
-									OnCommand=cmd(zoomy,0.475;zoomx,0.666);
-									LifeChangedMessageCommand=function(self) 
-									if lifeMaster <= 0.2 then self:setstate(1) self:visible(true) else self:setstate(0) end;
-									self:x((lifeMaster-0.5)*(SCREEN_WIDTH-102) + (SCREEN_WIDTH/2));
-									end;
-							};
-				
-else
+			-- steps
+			Def.BitmapText{
+				Font = Fonts.gameplay["Main"];
+				InitCommand=cmd(zoom,0.375;strokecolor,0.15,0.15,0.15,0.4;horizalign,pnAlign(pn);y,12;vertalign,bottom);
+				OnCommand=function(self)
+					local steps = Global.pncursteps[pn] or GAMESTATE:GetCurrentSteps(pn);
+					local tint = StepsColor(steps);
+					local meter = "00";
+					if TotalNotes(steps) > 0 then
+						meter = FormatMeter(steps:GetMeter());
+					end;
 
-			for pn in ivalues(source) do
+					self:diffuse(tint);
+					self:diffusetopedge(BoostColor(tint,9));
+					self:strokecolor(BoostColor(tint,0.2));
+					self:settext(PureType(steps).." "..meter);
 
-					t[#t+1] = Def.ActorFrame{
-					InitCommand=cmd(horizalign,left;);
-					OnCommand=function(self) 
-						if pn == PLAYER_1 then self:zoomx(1); self:x(SCREEN_LEFT+40);
-						elseif pn == PLAYER_2 then self:zoomx(-1); self:x(SCREEN_RIGHT-40); end; end;	
-						
-						
-						
-						
-							LoadActor("basemeter")..{
-								InitCommand=cmd(horizalign,left;y,posY-8;zoomy,-0.45;;zoomx,(((SCREEN_WIDTH-130)/400)/2);diffuse,0.4,0.4,0.4,0.7;animate,false);
-								LifeChangedMessageCommand=function(self) 
-								if pn	 == PLAYER_1 then lifeSelf = lifeP1; 
-								self:player(pn);
-								elseif pn == PLAYER_2 then lifeSelf = lifeP2; end;
-								if lifeSelf <= 0.2 then self:setstate(1); else self:setstate(0); end; end;
-								};
-			
-			
-							LoadActor("basemeter")..{
-								InitCommand=cmd(x,14;y,posY+5;horizalign,right;zoom,0.475;;zoomx,10/400;blend,Blend.Add;animate,false);
-							};
-			
-			
-							 Def.Quad{
-								InitCommand=cmd(y,posY;x,0.5*((SCREEN_WIDTH-120)/2);horizalign,left;zoomto,40,20;horizalign,left;bounce;effectmagnitude,-40,0,0;effectclock,"bgm";effecttiming,1,0,0,0;MaskSource;);
-								LifeChangedMessageCommand=function(self)
-									self:stoptweening();
-									self:decelerate(0.25);
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end; 
-									self:x(  8+  lifeSelf*((SCREEN_WIDTH-120)/2)    );
-									if lifeSelf == 1 then self:visible(false) else self:visible(true) end;
-								end;
-							};
+				end;
+			},
+		};
 
-							
-							LoadActor("basemeter")..{
-								InitCommand=cmd(x,14;y,posY+5;horizalign,left;zoom,0.475;;zoomx,((SCREEN_WIDTH-156)/400)/2;cropright,0.5;MaskDest;blend,Blend.Add;animate,false);
-								LifeChangedMessageCommand=function(self)
-								local lifeSelf;
-								if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end; 
-								self:cropright(1-lifeSelf);
-								--if lifeSelf <= 0.2 then self:setstate(1) else self:setstate(0); end;
-								end;
-							};
 
-							
-							LoadActor("hot_lores")..{
-									InitCommand=cmd(y,math.ceil(posY);horizalign,left;diffuse,0.5,0.75,0.95,0);
-									OnCommand=cmd(zoomto,(SCREEN_WIDTH-130)/2,39.1;customtexturerect,0,0,1,1;blend,Blend.Add;texcoordvelocity,0.33,0);
-									LifeChangedMessageCommand=function(self) 
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end; 
-									if lifeSelf == 1 then self:stoptweening(); self:decelerate(1); self:diffusealpha(1);
-									else self:stoptweening(); self:decelerate(0.1); self:diffusealpha(0); end;
-									end;
-							};
+		t[#t+1] = LoadActor("assets/lifebar")..{
+			OnCommand=cmd(playcommand, "Build", { Player = pn });
+		}
 
-			
-							LoadActor("mask")..{
-							InitCommand=cmd(y,posY;zoomx,0.475;zoomy,0.42;horizalign,left;x,-50);
-							};
-						
-							
-							 LoadActor("corner")..{
-									InitCommand=cmd(y,posY-0.5;zoom,0.375;horizalign,left);
-									LifeChangedMessageCommand=function(self) 
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end;
-									if lifeSelf <= 0.2 then self:diffuseshift(); self:effectcolor1(1,1,1,1); self:effectcolor2(1,0.5,0.33,1); self:effectperiod(0.05); else self:stopeffect(); end; end;
-							};
-							
-							
-							LoadActor("frame")..{
-									InitCommand=cmd(y,posY-0.5;horizalign,left;x,37.5);
-									OnCommand=cmd(zoomto,(SCREEN_WIDTH-238)/2-10,100*0.375;customtexturerect,0,0,1,1);
-									LifeChangedMessageCommand=function(self) 
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end; 
-									if lifeSelf <= 0.2 then self:diffuseshift(); self:effectcolor1(1,1,1,1); self:effectcolor2(1,0.5,0.33,1); self:effectperiod(0.05); else self:stopeffect(); end; end;
-							};
-							
-							
-							 LoadActor("center")..{
-									InitCommand=cmd(y,posY-0.5;zoom,0.375;zoomx,-0.375;horizalign,left;x,(SCREEN_WIDTH-108)/2);
-									LifeChangedMessageCommand=function(self) 
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1; elseif pn == PLAYER_2 then lifeSelf = lifeP2; end;
-									if lifeSelf <= 0.2 then self:diffuseshift(); self:effectcolor1(1,1,1,1); self:effectcolor2(1,0.5,0.33,1); self:effectperiod(0.05); else self:stopeffect(); end; end;
-							};
-											
-							LoadActor("tip")..{
-									InitCommand=cmd(y,math.ceil(posY)+5;animate,false;;blend,Blend.Add);
-									OnCommand=cmd(zoomy,0.475;zoomx,0.666;x,12+ 0.5*((SCREEN_WIDTH-160)/2)  );
-									LifeChangedMessageCommand=function(self) 
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end;
-									if lifeSelf == 0 then self:visible(false); else self:visible(true); end;
-									if lifeSelf <= 0.2 then self:setstate(1) self:visible(true) else self:setstate(0) end;
-									self:x(  12+ lifeSelf*((SCREEN_WIDTH-160)/2)    );
-									end;
-							};
-							LoadActor("tip")..{
-									InitCommand=cmd(y,math.ceil(posY)+5;animate,false;diffusealpha,0.25);
-									OnCommand=cmd(zoomy,0.475;zoomx,0.666;x,12+ 0.5*((SCREEN_WIDTH-160)/2) );
-									LifeChangedMessageCommand=function(self) 
-									local lifeSelf;
-									if pn == PLAYER_1 then lifeSelf = lifeP1 elseif pn == PLAYER_2 then lifeSelf = lifeP2 end;
-									if lifeSelf == 0 then self:visible(false); else self:visible(true); end;
-									if lifeSelf <= 0.2 then self:setstate(1) self:visible(true) else self:setstate(0) end;
-									self:x(  12+ lifeSelf*((SCREEN_WIDTH-160)/2)    );
-									end;
-							};
-							
-							
-				
-				
-					};
-		
-			end	
+	end;
 end;
 
-]]
+-- progress bar
+local p_height = 2;
+t[#t+1] = Def.ActorFrame{
+	Def.Quad{
+		InitCommand=cmd(diffuse,0,0,0,0.25;zoomto,SCREEN_WIDTH,p_height+2;horizalign,left;x,0;vertalign,top;y,SCREEN_TOP);
+	},
 
---[[
-t[#t+1] = Def.Quad{
-	OnCommand=function(self,params)
-		local s = SCREENMAN:GetTopScreen();
-		local player = s:GetChild("Player" .. ToEnumShortString(Global.master))
-		local field = player:GetChild("NoteField");
-		self:x(player:GetX());
-		self:y(player:GetY());
-		self:zoomto(field:get_width(), 16);
-		self:visible(false);
-	end;
-}
-]]--
+	Def.Quad{
+		InitCommand=cmd(zoomto,0,2;horizalign,left;x,1;vertalign,top;y,SCREEN_TOP+1);
+		OnCommand=cmd(diffuse,HighlightColor();;diffuserightedge,BoostColor(HighlightColor(),2));
+		UpdateMessageCommand=function(self)
+			if not paused then
+				local current = clamp(GAMESTATE:GetCurMusicSeconds(),0,math.huge);
+				local total = clamp(Global.song:GetStepsSeconds(),1,math.huge);
+				self:zoomto((SCREEN_WIDTH-2) * (current/total),p_height);
+			end;
+		end;
+	},
+};
+
+t[#t+1] = Def.BitmapText{
+	Font = Fonts.gameplay["Main"];
+	Text = "Stage "..CapDigits(GAMESTATE:GetCurrentStageIndex()+1,0,2);
+	InitCommand=cmd(zoom,0.45;strokecolor,0.15,0.15,0.15,0.8;CenterX;y,SCREEN_BOTTOM-12;vertalign,bottom);
+};
+
 
 t[#t+1] = LoadActor("assets/pause");
 
